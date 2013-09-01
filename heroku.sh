ccc9960cefbb76be544c74349b694bd9ea9378e2
Gennady Denisov <denisovgena@gmail.com>
2013-09-01 17:47:39 +0400

    add heroku base settings and requirements

---
+++ heroku.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+heroku config:add SECRET_KEY=`cat secret.txt`
+heroku config:add DJANGO_SETTINGS_MODULE=hotdot.settings.staging
+heroku config:add PYTHONPATH=$PYTHONPATH:./hotdot/hotdot
