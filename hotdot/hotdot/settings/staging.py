4be7d5f3c37ee7fdc769971fddc3879f1777083c
Gennady Denisov <denisovgena@gmail.com>
2013-09-01 19:43:51 +0400

    adjusting static files configuration

--- hotdot/hotdot/settings/staging.py
+++ hotdot/hotdot/settings/staging.py
@@ -22,8 +22,3 @@ DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-# Static asset configuration
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
