b8b4a6013ef39e036acddc09e6ac2cfa33c183ce
Gennady Denisov <geaden@yandex-team.ru>
2023-03-30 14:02:04 +0400

    fix patterns import

--- hotdot/apps/tags/urls.py
+++ hotdot/apps/tags/urls.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import PostsByTagView, AjaxTagsAutocomplete
 
 __author__ = 'Gennady Denisov <denisovgena@gmail.com>'
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^tag/(?P<pk>\d+)$', PostsByTagView.as_view(), name='detail'),
     url(r'^tags/autocomplete/$', AjaxTagsAutocomplete.as_view(), name='autocomplete'),
-
-    )
+]
